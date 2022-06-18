ee5de719d84b408a1c8b87dba0dea203e146dfd6
Rascalitoes <55996478+Rascalitoes@users.noreply.github.com>
2022-06-18 19:33:17 -0400

    Item now extends Pair
    Left in the old code as comments just in case

--- src/Item.java
+++ src/Item.java
@@ -1,23 +1,36 @@
+import javafx.util.Pair;
+
 import java.math.BigDecimal;
 
-public class Item implements java.io.Serializable{
-    private String name;
-    private BigDecimal amount;
+public class Item extends Pair<String,BigDecimal> implements java.io.Serializable{
+//    private String name;
+//    private BigDecimal amount;
     // Category when that's done
     // Also option for item to be returned
     // Basically a K-V Pair
 
     public Item(String description, BigDecimal amount){
-        this.name = description;
-        this.amount = amount;
+        super(description,amount);
+//        this.name = description;
+//        this.amount = amount;
+    }
+
+    public Item(String description, String amount){
+        super(description,new BigDecimal(amount));
+//        this.name = description;
+//        this.amount = new BigDecimal(amount);
+    }
+
+    public String getName(){
+        return this.getKey();
     }
 
     public BigDecimal getAmount(){
-        return this.amount;
+        return this.getValue();
     }
 
     public String toString(){
-        return name + "\t\t" + amount.toString();
+        return this.getKey() + "\t\t" + this.getValue();
     }
 
 }
