5cce2dc7d3c0d021d1670eb365649d34c391f930
Rascalitoes <55996478+Rascalitoes@users.noreply.github.com>
2022-06-24 18:36:23 -0400

    ListedView now pulls from Db

--- src/ListedView.java
+++ src/ListedView.java
@@ -1,6 +1,8 @@
+import javafx.collections.FXCollections;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
@@ -20,13 +22,18 @@ public class ListedView extends GridPane {
         add(receiptTable,0,1,2,1);
 
         TableColumn dateCol = new TableColumn("Date");
+        dateCol.setCellValueFactory(new PropertyValueFactory<Receipt,String>("date"));
         TableColumn storeCol = new TableColumn("Store");
+        storeCol.setCellValueFactory(new PropertyValueFactory<Receipt,String>("store"));
         TableColumn itemCol = new TableColumn("Items");
+        itemCol.setCellValueFactory(new PropertyValueFactory<Receipt,String>("items"));
         TableColumn subtotalCol = new TableColumn("Subtotal");
+        subtotalCol.setCellValueFactory(new PropertyValueFactory<Receipt,String>("subtotal"));
         TableColumn totalCol = new TableColumn("Total");
+        totalCol.setCellValueFactory(new PropertyValueFactory<Receipt,String>("total"));
         receiptTable.getColumns().addAll(dateCol,storeCol,itemCol,subtotalCol,totalCol);
 
-
+        receiptTable.setItems(FXCollections.observableArrayList(new Receipt(),new Receipt()));
 
         setHgap(10);
         setVgap(5);
